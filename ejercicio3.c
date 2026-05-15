330716f26252edbf2cfead443610d7cf9630211b
Jordan98-San <jordan.sanchez@udla.edu.ec>
2026-05-15 15:13:44 -0500

    Primer commit del tercer ejercicio

--- ejercicio3.c
+++ ejercicio3.c
@@ -2,13 +2,13 @@
 
 int main(){
     char vocales[6] = { 'a',  'e',  'i',  'o',  'u'};
-    //char *ptr;
-    //ptr = vocales;
+    char *ptr;
+    ptr = vocales;
 
     for (int i = 0; i < 6; i++)
     {
-         printf("%c\t", vocales[i]);
-       // printf("%c\t", *(ptr+i));
+        
+        printf("%c\t", *(ptr+i));
     }
     
 }
