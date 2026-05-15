ca96da5eca95ca17b2079bef1eb71048961ffb08
Jordan98-San <jordan.sanchez@udla.edu.ec>
2026-05-15 14:37:20 -0500

    impresion de memoria

--- main.c
+++ main.c
@@ -4,14 +4,19 @@ int main(void){
     int a[] = {3,6,9,12,15,18};
     
     int* ptr;
-     ptr = &a[3];
+    ptr = &a[0];
+   
    
     for (int i = 0; i < 6; i++)
     {
-        printf("%d\t", a[i]);
+         // ptr = &a[i];
+         
+          printf("arreglo[%d] = %d ", i, a[i]);
+        printf("%d\n", *(ptr+i));
+       // printf("%p\t", ptr);
     }
     
-    printf("\na = %p\n", ptr);
-    printf("a = %d\n", *ptr);
+    //printf("\na = %p\n", ptr);
+   // printf("a = %d\n", *ptr);
     return 0;
 }
